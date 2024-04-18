0db24ef0fceb2e3f6a7db0a9b3253ce5eb826442
changraekang <storkcr@gmail.com>
2024-04-18 10:13:35 +0900

    body-parser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
 //앱은 서버를 시작하며 3000번 포트에서 연결을 청취함
 const port = 3000;
