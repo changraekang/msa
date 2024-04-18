fa268b323b4d1f050134fe3096814223dfae2747
changraekang <storkcr@gmail.com>
2024-04-18 17:41:04 +0900

    redis 추가

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const testRoute = require("./routes/test.js");
+const { createClient } = require("redis");
 
+let client; // 전역 변수로 설정
+
+async function run() {
+  client = createClient({ url: "redis://localhost", port: 6379 });
+
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  try {
+    await client.connect();
+    console.log("Connected to Redis");
+  } catch (err) {
+    console.error("Could not connect to Redis:", err);
+  }
+}
+
+run();
 const app = express();
 //앱은 서버를 시작하며 3000번 포트에서 연결을 청취함
 const port = 3000;
